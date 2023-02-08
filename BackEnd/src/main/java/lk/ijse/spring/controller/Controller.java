d36cbcd61b9c19cda5cc88e851c78fd509eb969e
TharuZika DilakZan <maxtharuz4@gmail.com>
2023-02-09 01:29:09 +0530

    Configure Controller

--- BackEnd/src/main/java/lk/ijse/spring/controller/Controller.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/Controller.java
@@ -1,4 +1,74 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import javax.servlet.http.Part;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/upload")
 public class Controller {
+
+    private static String imgSrc = new String();
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity fileUploading(@RequestPart("myFile") MultipartFile myFile, @RequestPart("myFile") byte[] isFile, @RequestPart("myFile") Part myPart) {
+
+        try {
+            // Let's get the project location
+            // tomcat/webapps/uploads (files uploaded directory)
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+
+            // Let's create a folder there for uploading purposes, if not exists
+            File uploadsDir = new File(projectPath + "/uploads");
+            uploadsDir.mkdir();
+
+            // It is time to transfer the file into the newly created dir
+            myFile.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + myFile.getOriginalFilename()));
+
+            return new ResponseEntity("Successfully Uploaded", HttpStatus.OK);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity uploadFileWithSpringWay(@RequestPart("myFile") MultipartFile myFile) {
+        try {
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadsDir = new File(projectPath + "/uploads");
+            System.out.println(projectPath);
+            uploadsDir.mkdir();
+            myFile.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + myFile.getOriginalFilename()));
+
+            //save the path of the uploaded image in the temporary database
+            imgSrc = "uploads/" + myFile.getOriginalFilename();
+
+            return ResponseEntity.ok(HttpStatus.OK);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getAllImagesFromDatabase() {
+        return new ResponseEntity(imgSrc, HttpStatus.OK);
+    }
 }

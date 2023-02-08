b63a7b5f96d2e02c65d8ef98e07cc651b642e7f7
TharuZika DilakZan <maxtharuz4@gmail.com>
2023-02-09 01:09:45 +0530

    Create Config file and Initializer

---
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+public class WebRootConfig {
+}
